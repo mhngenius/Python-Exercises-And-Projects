d8e47d1fddb8fc6f6d75e3874c9ca4f338c77264
Mahan Mehrabi <gnusmhn@gmail.com>
2025-05-23 19:44:54 +0330

    Add files via upload

---
+++ 001.First 5 lessons of the python's tutorial repo (1-5)/002.Fun fact/main.py
@@ -0,0 +1,7 @@
+# Ask the user for their name and a number,
+# then print a funny little “fact” about that number using variables and input.
+
+name = input("Enter your name:")
+number = input("Enter a number:")
+
+print(f"Hi {name}!. Did know that {number} is your lucky number? 😁")
